2427fb7fdc01a0c27fbf1ab9beb3b1a74fcff2f5
Shiwomi13 <yaroslavaleksandrov11@mail.ru>
2021-10-25 22:35:12 +0300

    lab2

---
+++ lab/A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class A
+    {
+        protected List<double> X1;
+        public List<double> LinearEquation(double a, double b)
+        {
+            if (a == 0)
+                return null;
+
+            return X1 = new List<double>(){ 
+                -b / a 
+            };
+
+        }
+    } 
+}
+
---
+++ lab/B.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class B : A
+    {
+        public List<double> QuadraticEquation(double a, double b, double c)
+        {
+            double D = SearchDiscriminate(a, b, c);
+
+            if (a == 0)
+            {
+                return LinearEquation(b, c);
+            }
+
+            if (D < 0)
+            {
+                return null;
+            }
+
+            if (D == 0)
+            {
+                return X1 = new List<double>(){
+                    (-b) / (2*a)
+                };
+            }
+
+            return X1 = new List<double>(){
+                (-b + Math.Sqrt(D)) / (2 * a),
+                (-b - Math.Sqrt(D)) / (2 * a)
+            };
+        }
+        protected double SearchDiscriminate(double a, double b, double c)
+        {
+            return b * b - (4 * a * c);
+        }
+    }
+}
